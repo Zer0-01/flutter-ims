af1f07ae1482709d5480c66060a3ae4c8bb92b69
Anas <anas.zulkifli2001@gmail.com>
2025-09-24 23:43:18 +0800

    feat: update repostitory

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -2,23 +2,20 @@ import 'package:flutter_ims/configuration/app_api_service.dart';
 import 'package:flutter_ims/configuration/app_logger.dart';
 import 'package:flutter_ims/data/api_endpoints.dart';
 import 'package:flutter_ims/data/models/request/register_dto_request.dart';
-import 'package:flutter_ims/data/models/response/register_dto_response.dart';
 
 class UserRepository {
   final _logger = AppLogger.getLogger("UserRepository");
 
-  Future<RegisterDtoResponse> postRegister({
+  Future<void> postRegister({
     required RegisterDtoRequest registerDtoRequest,
   }) async {
     _logger.debug("postRegister");
     _logger.debug("registerDtoRequest: ${registerDtoRequest.toJson()}");
 
-    final response = await AppApiService.instance.request(
+    await AppApiService.instance.request(
       ApiEndpoints.registerOrganization,
       DioMethod.post,
       param: registerDtoRequest.toJson(),
     );
-
-    return RegisterDtoResponse.fromJson(response.data);
   }
 }
