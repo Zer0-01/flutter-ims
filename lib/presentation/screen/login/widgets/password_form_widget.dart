047c08e22c696c0a1d64101d591ffb6f64649899
Anas <anas.zulkifli2001@gmail.com>
2025-09-28 13:02:15 +0800

    feat: obsucre text

--- lib/presentation/screen/login/widgets/password_form_widget.dart
+++ lib/presentation/screen/login/widgets/password_form_widget.dart
@@ -1,16 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_ims/presentation/common_widgets/app_text_form_field_widget.dart';
 import 'package:flutter_ims/utils/extension.dart';
 
-class PasswordFormWidget extends StatelessWidget {
+class PasswordFormWidget extends StatefulWidget {
   final TextEditingController passwordController;
 
   const PasswordFormWidget({super.key, required this.passwordController});
 
   @override
+  State<PasswordFormWidget> createState() => _PasswordFormWidgetState();
+}
+
+class _PasswordFormWidgetState extends State<PasswordFormWidget> {
+  bool isObscure = true;
+
+  @override
   Widget build(BuildContext context) {
     return AppTextFormFieldWidget(
-      controller: passwordController,
+      controller: widget.passwordController,
+      obscureText: isObscure,
+      suffixIcon: GestureDetector(
+        onTap: () {
+          setState(() {
+            isObscure = !isObscure;
+          });
+        },
+        child: Icon(isObscure ? Icons.visibility_off : Icons.visibility),
+      ),
       label: Text(context.l10n.password),
     );
   }
